27e4f832df9536e9b96006289a3260baa79102a9
Cesar Guerron <guerronc@outlook.com>
2020-04-10 11:57:59 -0500

    Modify import to scss file

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,7 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import '../assets/styles/App.scss';
+import '../assets/styles/app.scss';
 
 const Home = ({ mylist, trends, originals }) => {
   return (
